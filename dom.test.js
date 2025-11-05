ef2bb1e676915f24f022199162794c63701035ff
billyquizboi <186437385+billyquizboi@users.noreply.github.com>
2025-11-04 20:24:38 -0500

    Remove jest related testing code

--- dom.test.js
+++
@@ -1,9 +0,0 @@
-// dom.test.js
-describe('DOM manipulation', () => {
-  test('should add a class to an element', () => {
-    document.body.innerHTML = '<div id="myElement"></div>';
-    const element = document.getElementById('myElement');
-    element.classList.add('new-class');
-    expect(element.classList.contains('new-class')).toBe(true);
-  });
-});
--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  testEnvironment: 'jest-fixed-jsdom',
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  transformIgnorePatterns: ['node_modules/(?!(sucrase)/)'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
-  }
-};
